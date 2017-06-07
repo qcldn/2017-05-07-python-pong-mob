7ef65fe246ba3b92d46e5fb34d576b6762b12728
Hector Dearman <hjd@google.com>
2017-06-07 20:12:10 +0100

    Left paddle moves

--- pong.py
+++ pong.py
@@ -6,20 +6,24 @@ clock = pygame.time.Clock()
 
 WIDTH = 600
 HEIGHT = 400
+UP = 1
+DOWN = -1
 
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Pong')
 
 ball_center = (300, 200)
 velocity = (-1, 1)
+left_paddle_coords = (50, 175)
 
 def draw_the_ball(window, ball_center):
     window.fill((0, 0, 0))
     pygame.draw.circle(window, (0, 255, 0), ball_center, 10, 0)
 
 def draw_paddles(window):
-    pygame.draw.rect(window, (0,255,0),(50,175,10,50),0)
-    pygame.draw.rect(window, (0,255,0),(WIDTH - 60, HEIGHT/2 -25 ,10,50),0)
+    x, y = left_paddle_coords
+    pygame.draw.rect(window, (0,255,0), (x, y,10,50) ,0)
+    pygame.draw.rect(window, (0,255,0), (WIDTH - 60, HEIGHT/2 -25 ,10,50) ,0)
 
 def new_ball_center():
     (x, y) = ball_center
@@ -35,11 +39,25 @@ def detect_collision(ball_center):
         dy = dy*-1
     return (dx, dy)
 
+def move_left_paddle(direction):
+    x, y = left_paddle_coords
+    if direction == UP:
+        y = y - 1
+    elif direction == DOWN:
+        y = y + 1
+    return (x, y)
+
 while True:
     draw_the_ball(window, ball_center)
     draw_paddles(window)
     ball_center, velocity = new_ball_center()
     pygame.display.update()
     for event in pygame.event.get():
-        pass
+        if event.type == KEYDOWN:
+            print(event.key)
+            if event.key == K_w:
+                print("I'm working")
+                left_paddle_coords = move_left_paddle(UP)
+            elif event.key == K_s:
+                left_paddle_coords = move_left_paddle(DOWN)
     clock.tick(60)
